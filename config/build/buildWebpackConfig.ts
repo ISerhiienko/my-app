0110f0add3f0dcfd6b69ee09eb8d6c29943414ee
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-06-04 13:03:42 +0300

    feat: decompose the config, added options

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import { BuildOptions } from "./types/config";
+import webpack from 'webpack';
+import path from "path";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+
+  const {paths, mode} = options;
+
+  return {
+    mode,
+    entry: paths.entry,
+    module: {
+      rules: buildLoaders()
+    },
+    resolve: buildResolvers(),
+    output: {
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true
+    },
+    plugins: buildPlugins(options)
+  }
+  }
