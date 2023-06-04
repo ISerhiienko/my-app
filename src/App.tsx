8f2bc40191edf092985670e2965550dc6e05ec33
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-06-04 16:42:36 +0300

    feat: add routing to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Hello } from "./components/Hello";
+import { Routes, Route,Link  } from "react-router-dom";
 import './index.scss';
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPageLazy";
+import { Suspense } from 'react';
 
 export const App = () => {
   return (
     <div className="app">
-      <Hello />
+      <Link to="/">Main page</Link>
+      <Link to="/about ">About site</Link>
+
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+            <Route path={'/about'} element={<AboutPageLazy />} />
+            <Route path={'/'} element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
