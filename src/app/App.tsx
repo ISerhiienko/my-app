a9bb5170a1e0e3f108ca1e23757d2278c82ece53
Ihor Serhiienko <igorserhiyenko@gmail.com>
2023-06-04 19:10:05 +0300

    feat: configured the application architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Routes, Route,Link  } from "react-router-dom";
 import './styles/index.scss';
-import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
-import { MainPageLazy } from "./pages/MainPage/MainPageLazy";
 import { Suspense } from 'react';
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 
 export const App = () => {
   const {theme, toggleTheme} = useTheme();
@@ -18,8 +18,8 @@ export const App = () => {
 
       <Suspense fallback={<div>loading...</div>}>
         <Routes>
-            <Route path={'/about'} element={<AboutPageLazy />} />
-            <Route path={'/'} element={<MainPageLazy />} />
+            <Route path={'/about'} element={<AboutPage />} />
+            <Route path={'/'} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
--- src/components/Hello.tsx
+++
@@ -1,7 +0,0 @@
-import classes from "./Hello.module.scss";
-
-export const Hello = () => {
-  return (
-    <div className={classes.green}>Hello</div>
-  )
-}
