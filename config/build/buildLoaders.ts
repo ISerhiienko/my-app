d10530e9695a5db925b2d194fe48391faeb8ae73
Ihor Serhiienko <igorserhiyenko@gmail.com>
2025-02-13 10:38:06 +0200

    feat: add webpack svg, file loaders, Button component

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,20 +2,26 @@ import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildOptions } from "./types/config";
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
 
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
-      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
       {
-        loader: 'css-loader',
+        loader: "css-loader",
         options: {
           modules: {
-            auto: (resPath: string) => Boolean(resPath.includes('.module')),
-            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
-          }
-        }
+            auto: (resPath: string) => Boolean(resPath.includes(".module")),
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
       },
       "sass-loader",
     ],
@@ -23,12 +29,19 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
   const typescriptLoader = {
     test: /\.tsx?$/,
-    use: 'ts-loader',
+    use: "ts-loader",
     exclude: /node_modules/,
   };
 
-  return [
-    cssLoader,
-    typescriptLoader
-  ];
+  const fileLoader = {
+    test: /\.(png|jpg|gif|woff|woff2)$/,
+    use: [
+      {
+        loader: "file-loader",
+        options: {},
+      },
+    ],
+  };
+
+  return [fileLoader, svgLoader, cssLoader, typescriptLoader];
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,14 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import "./styles/index.scss";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 export const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-
-      <button onClick={toggleTheme}>TOGGLE</button>
 
       <AppRouter />
     </div>
