dc2bf4000d24901a3619c03a26ec1db7198e4895
Ihor Serhiienko <igorserhiyenko@gmail.com>
2025-02-17 10:47:48 +0200

    feat: add webpack bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,21 +1,23 @@
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BuildOptions } from './types/config';
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export function buildPlugins({
-    paths,
-    isDev,
+  paths,
+  isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
-        new webpack.ProgressPlugin(),
-        new HtmlWebpackPlugin({ template: paths.html }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
-    ];
+  return [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({ template: paths.html }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+    new BundleAnalyzerPlugin(),
+  ];
 }
