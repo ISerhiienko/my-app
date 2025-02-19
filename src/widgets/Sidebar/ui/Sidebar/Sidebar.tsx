5d85cc89bfcff7c3462e7c5203c0ea9d6095dbb2
Ihor Serhiienko <igorserhiyenko@gmail.com>
2025-02-19 22:52:20 +0200

    feat: add Button mods, update Button stories

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,7 +4,11 @@ import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
 import { useTranslation } from "react-i18next";
 import cls from "./Sidebar.module.scss";
-import { Button } from "../../../../shared/ui/Button/Button";
+import {
+  Button,
+  ButtonSize,
+  ButtonTheme,
+} from "../../../../shared/ui/Button/Button";
 
 interface SidebarProps {
   className?: string;
@@ -23,9 +27,17 @@ export const Sidebar = ({ className }: SidebarProps) => {
         className,
       ])}
     >
-      <Button data-testid="sidebar-toggle" onClick={onToggle}>
-        {t("Перемикач")}
+      <Button
+        data-testid="sidebar-toggle"
+        onClick={onToggle}
+        className={cls.collapseBtn}
+        theme={ButtonTheme.BACKGROUND_INVERTED}
+        size={ButtonSize.L}
+        square
+      >
+        {collapsed ? ">" : "<"}
       </Button>
+
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
