8e4620e16a11c7d0a56efd2f9975077001bfc73c
Ihor Serhiienko <igorserhiyenko@gmail.com>
2025-02-22 12:09:50 +0200

    feat: add Modal, Portal, modal stories, fix theme, update button theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,16 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
 import { useTheme } from "./providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import "./styles/index.scss";
+import { Modal } from "shared/ui/Modal/Modal";
 
 export const App = () => {
   const { theme } = useTheme();
+
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={classNames("app", {}, [theme])}>
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,13 +1,37 @@
 import { classNames } from "shared/lib/classNames/classNames";
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { t } from "i18next";
 import { useTranslation } from "react-i18next";
 import cls from "./Navbar.module.scss";
+import { Modal } from "shared/ui/Modal/Modal";
+import { Button, ButtonTheme } from "shared/ui/Button/Button";
+import { useCallback, useState } from "react";
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-  <div className={classNames(cls.Navbar, {}, [className])} />
-);
+export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
+
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <Button
+        theme={ButtonTheme.CLEAR_INVERTED}
+        className={cls.links}
+        onClick={onToggleModal}
+      >
+        {t("Увійти")}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        {/* eslint-disable-next-line */}
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
+        commodi consequatur eligendi impedit incidunt necessitatibus possimus
+        quis saepe sunt totam."
+      </Modal>
+    </div>
+  );
+};
