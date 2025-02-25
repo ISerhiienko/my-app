82e326c054c950e3fb7f47a6263a5ff4cf6c9cd4
Ihor Serhiienko <igorserhiyenko@gmail.com>
2025-02-25 12:36:12 +0200

    feat: add Input, add auth modal form ui

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { Modal } from "shared/ui/Modal/Modal";
 import { Button, ButtonTheme } from "shared/ui/Button/Button";
 import { useCallback, useState } from "react";
+import { LoginModal } from "features/AuthByUsername";
 import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
@@ -13,8 +14,12 @@ export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
   const [isAuthModal, setIsAuthModal] = useState(false);
 
-  const onToggleModal = useCallback(() => {
-    setIsAuthModal((prev) => !prev);
+  const onCloseModal = useCallback(() => {
+    setIsAuthModal(false);
+  }, []);
+
+  const onShowModal = useCallback(() => {
+    setIsAuthModal(true);
   }, []);
 
   return (
@@ -22,16 +27,11 @@ export const Navbar = ({ className }: NavbarProps) => {
       <Button
         theme={ButtonTheme.CLEAR_INVERTED}
         className={cls.links}
-        onClick={onToggleModal}
+        onClick={onShowModal}
       >
         {t("Увійти")}
       </Button>
-      {/* eslint-disable-next-line i18next/no-literal-string */}
-      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid
-        commodi consequatur eligendi impedit incidunt necessitatibus possimus
-        quis saepe sunt totam.
-      </Modal>
+      <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
     </div>
   );
 };
