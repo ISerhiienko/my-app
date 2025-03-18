b4d2f468f055622a3e48b368248c5943b2d6b1e2
Ihor Serhiienko <igorserhiyenko@gmail.com>
2025-03-18 21:44:44 +0200

    feat: fetching articles, add view type

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,54 +1,56 @@
 import { classNames } from "shared/lib/classNames/classNames";
-import { useTranslation } from "react-i18next";
-import { memo } from "react";
-import { Article, ArticleList, ArticleView } from "entities/Article";
+import { memo, useCallback } from "react";
+import { ArticleList, ArticleView, ArticleViewSelector } from "entities/Article";
+import { DynamicModuleLoader, ReducersList } from "shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
+import { useAppDispatch } from "shared/lib/hooks/useAppDispatch/useAppDispatch";
+import { useInitialEffect } from "shared/lib/hooks/useInitialEffect/useInitialEffect";
+import { useSelector } from "react-redux";
+import {
+    getArticlesPageError,
+    getArticlesPageIsLoading,
+    getArticlesPageView,
+} from "pages/ArticlesPage/model/selectors/articlesPageSelectors";
+import { fetchArticlesList } from "../../model/services/fetchArticlesList/fetchArticlesList";
 import cls from "./ArticlesPage.module.scss";
+import { articlesPageActions, articlesPageReducer, getArticles } from "../../model/slices/articlesPageSlice";
 
 interface ArticlesPageProps {
     className?: string;
 }
 
+const reducers: ReducersList = {
+    articlesPage: articlesPageReducer,
+};
+
 const ArticlesPage = (props: ArticlesPageProps) => {
     const { className } = props;
-    const { t } = useTranslation();
 
-    const article = {
-        id: "1",
-        title: "Javascript news",
-        subtitle: "What's new in JS for 2025?",
-        img: "https://teknotower.com/wp-content/uploads/2020/11/js.png",
-        views: 1022,
-        createdAt: "03.09.2025",
-        user: {
-            id: "1",
-            username: "ihor",
-        },
-        type: [
-            "IT",
-            "POLITICS",
-        ],
-        blocks: [
-            {
-                id: "1",
-                type: "TEXT",
-                title: "Block Title",
-                paragraphs: [
-                    "The program traditionally called 'Hello, world!' is very simple. "
-                    + "It outputs the phrase 'Hello, world!' "
-                    + "or something similar, using some programming language.",
-                ],
-            },
-        ],
-    } as Article;
+    const dispatch = useAppDispatch();
+    const articles = useSelector(getArticles.selectAll);
+    const isLoading = useSelector(getArticlesPageIsLoading);
+    const view = useSelector(getArticlesPageView);
+    const error = useSelector(getArticlesPageError);
+
+    const onChangeView = useCallback((view: ArticleView) => {
+        dispatch(articlesPageActions.setView(view));
+    }, [dispatch]);
+
+    useInitialEffect(() => {
+        dispatch(fetchArticlesList());
+        dispatch(articlesPageActions.initState());
+    });
 
     return (
-        <div className={classNames(cls.ArticlesPage, {}, [className])}>
-            <ArticleList
-                isLoading
-                view={ArticleView.BIG}
-                articles={[article]}
-            />
-        </div>
+        <DynamicModuleLoader reducers={reducers}>
+            <div className={classNames(cls.ArticlesPage, {}, [className])}>
+                <ArticleViewSelector view={view} onViewClick={onChangeView} />
+                <ArticleList
+                    isLoading={isLoading}
+                    view={view}
+                    articles={articles}
+                />
+            </div>
+        </DynamicModuleLoader>
     );
 };
 
