98a0daff10bf612ca6e1553a44c05283348e686d
Ihor Serhiienko <igorserhiyenko@gmail.com>
2025-04-03 16:41:41 +0300

    feat: add recomendation articles for article details page

--- src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentsSlice.ts
+++ src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentsSlice.ts
@@ -2,7 +2,6 @@ import {
     createEntityAdapter,
     createSlice, PayloadAction,
 } from "@reduxjs/toolkit";
-
 import { Comment } from "entities/Comment";
 import { StateSchema } from "app/providers/StoreProvider";
 import {
@@ -15,7 +14,7 @@ const commentsAdapter = createEntityAdapter<Comment>({
 });
 
 export const getArticleComments = commentsAdapter.getSelectors<StateSchema>(
-    (state) => state.articleDetailsComments || commentsAdapter.getInitialState(),
+    (state) => state.articleDetailsPage?.comments || commentsAdapter.getInitialState(),
 );
 
 const articleDetailsCommentsSlice = createSlice({
